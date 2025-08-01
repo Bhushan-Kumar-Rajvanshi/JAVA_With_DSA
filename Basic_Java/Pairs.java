7b0a2d28b86849d31f65cdea2f4dd3c77640b9af
Bhushan-Kumar-Rajvanshi <bhushan1may@gmail.com>
2025-08-01 07:01:43 +0530

    Pairs print between 0 and 10

---
+++ Basic_Java/Pairs.java
@@ -0,0 +1,19 @@
+package Basic_Java;
+
+public class Pairs {
+    public static void main(String[] args) {
+        // int i = 0, j = 10;        
+        // while (i < j) {
+        //     System.out.println("Pair: (" + i + ", " + j + ")");
+        //     i++;
+        //     j--;
+        // }
+
+
+
+        // Using a for loop to generate pairs
+        for(int i=0, j=10; i<j; i++, j--){
+                    System.out.println("Pair: (" + i + ", " + j + ")");
+        }
+    }
+}
