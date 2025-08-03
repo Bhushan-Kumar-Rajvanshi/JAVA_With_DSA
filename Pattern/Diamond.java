c3449eb3c5f3bec9e463113862bfabc097ac8c6a
Bhushan-Kumar-Rajvanshi <rajdevram7070@gmail.com>
2025-08-03 16:11:33 +0530

    Diamond Pattern

---
+++ Pattern/Diamond.java
@@ -0,0 +1,39 @@
+package Pattern;
+import java.util.Scanner;
+
+
+public class Diamond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number to print the pattern: ");
+        int num = sc.nextInt();
+        int totalLines = num*2 - 1;
+        int stars = 1;
+        int space = num - 1;
+        int currentLine = 1;
+
+        while(currentLine<=totalLines) {
+            // Print spaces
+            for(int i=1; i<=space; i++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for(int i=1; i<=stars; i++) {
+                System.out.print("*");
+            }
+            // Move to the next line
+            System.out.println();
+
+            // Update stars and spaces for the next line
+            if(currentLine < num) {
+                stars += 2;
+                space--;
+            } else {
+                stars -= 2;
+                space++;
+            }
+            currentLine++;
+        }
+        sc.close();
+    }
+}
